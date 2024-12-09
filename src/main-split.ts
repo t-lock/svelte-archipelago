af69ac57ce7488f9ccb3ad6695f857573f0beabc
Tim Wheelock <tlock@armyofbees.com>
2024-12-09 15:39:03 +0900

    Project direction is settled, remove all other experimentation

--- src/main-split.ts
+++
@@ -1,5 +0,0 @@
-// import { hydrate } from "svelte";
-
-const modules = import.meta.glob("./lib/**/*.svelte");
-
-Object.keys(modules).forEach((path) => modules[path]());
--- src/renderCounter.ts
+++
@@ -1,8 +0,0 @@
-import { render } from "svelte/server";
-import Counter from "./lib/Counter.svelte";
-
-const result = render(Counter, {
-  props: {},
-});
-result.body; // HTML for somewhere in this <body> tag
-result.head; // HTML for somewhere in this <head> tag
